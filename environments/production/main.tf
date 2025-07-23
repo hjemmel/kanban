8fea7e4fde4a07e013941dc2be0dc8be0a2340ff
Hans Jakob Emmel <547867+hjemmel@users.noreply.github.com>
2025-07-23 23:15:34 +1000

    feat: Create a Continuous Delivery pipeline (#28)

--- environments/production/main.tf
+++ environments/production/main.tf
@@ -3,6 +3,20 @@ module "swarm" {
   private_key_path = "${path.module}/private_key.pem"
 }
 
+module "repository_secrets" {
+  source = "../../modules/integrations/github/secrets"
+  secrets = {
+    "PRIVATE_KEY"           = module.swarm.private_key,
+    "AWS_ACCESS_KEY_ID"     = var.aws_access_key_id,
+    "AWS_SECRET_ACCESS_KEY" = var.aws_secret_access_key,
+    "AGE_KEY"               = var.age_key,
+    "GH_PAT"                = var.gh_pat,
+    "CODE_COV_TOKEN"        = var.code_cov
+  }
+  repository   = "kanban"
+  github_owner = "hjemmel"
+}
+
 output "swarm_ssh_command" {
   value = module.swarm.ssh_command
 }
---
+++ environments/production/variables.tf
@@ -0,0 +1,25 @@
+variable "aws_access_key_id" {
+  description = "The AWS access key ID."
+  type        = string
+}
+variable "aws_secret_access_key" {
+  description = "The AWS secret access key."
+  type        = string
+  sensitive   = true
+}
+variable "age_key" {
+  description = "The age key for sops encryption tool"
+  type        = string
+  sensitive   = true
+}
+variable "gh_pat" {
+  description = "A GitHub personal access token. Used to log into ghcr.io."
+  type        = string
+  sensitive   = true
+}
+
+variable "code_cov" {
+  description = "Code coverage token to upload coverage reports to codecov.io"
+  type        = string
+  sensitive   = true
+}
